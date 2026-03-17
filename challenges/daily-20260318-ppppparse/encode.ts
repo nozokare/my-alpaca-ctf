1e66c419a627a1e280a91fed2b35ee4c083b41ef
nozokare <contact@nozokare.com>
2026-03-18 01:36:23 +0900

    solved.

---
+++ challenges/daily-20260318-ppppparse/encode.ts
@@ -0,0 +1,8 @@
+let input = "42";
+input = JSON.stringify(input);
+input = JSON.stringify(input);
+input = JSON.stringify(input);
+input = JSON.stringify(input);
+input = JSON.stringify(input);
+console.log(input);
+console.log(input.replaceAll("\\\\", "\\u005c"));
