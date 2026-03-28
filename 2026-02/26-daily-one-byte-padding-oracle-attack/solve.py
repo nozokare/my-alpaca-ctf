462563165c1a889bb67409f85dc4a604f691cf8e
nozokare <contact@nozokare.com>
2026-03-28 18:42:07 +0900

    add writeup

--- 2026-02/26-daily-one-byte-padding-oracle-attack/solve.py
+++ 2026-02/26-daily-one-byte-padding-oracle-attack/solve.py
@@ -8,7 +8,7 @@ assert nc == "nc" and host and port.isnumeric()
 
 # from local import iv_ciphertext, decrypt
 
-# def oracle(data: bytes) -> bool:
+# def is_valid_padding(data: bytes) -> bool:
 #     return decrypt(data[:16], data[16:])
 
 from pwn import remote
@@ -17,12 +17,11 @@ conn = remote(host, port)
 line = conn.recvline().decode()
 iv_ciphertext = bytes.fromhex(line.split("=")[1])
 
-bs = 16
-bc = len(iv_ciphertext) // bs
-blocks = [iv_ciphertext[i * bs : (i + 1) * bs] for i in range(bc)]
+count = len(iv_ciphertext) // 16
+blocks = [iv_ciphertext[i * 16 : (i + 1) * 16] for i in range(count)]
 
 
-def oracle(data: bytes) -> bool:
+def is_valid_padding(data: bytes) -> bool:
     conn.sendlineafter(b"> ", data.hex().encode())
     result = conn.recvline().strip().decode()
     return result == "True"
@@ -32,15 +31,19 @@ def decrypt_block_char(Ci_1: bytes, Ci: bytes):
     data = bytearray([0] * 16) + Ci
     for b in range(256):
         data[15] = b
-        if oracle(data):
-            c = chr(data[15] ^ 0x01 ^ Ci_1[15])
-            if c.isprintable():
-                return c
+        if is_valid_padding(data):
+            # 末尾から 2 バイト目を変えてパディングが崩れるなら
+            # 末尾は `\x01` ではないので別の候補を探す
+            data[14] = 1 
+            if not is_valid_padding(data):
+                continue
+
+            return chr(data[15] ^ 0x01 ^ Ci_1[15])
 
 
 # [iv][A][l][p][a][c][a][{]...[}][pad]
 flag = "Alpaca{"
-for i in range(8, bc - 2):
+for i in range(8, count - 2):
     flag += decrypt_block_char(blocks[i - 1], blocks[i])
 flag += "}"
 print(flag)
